eef59a8037dd5f241005b8483386d4ae4f912db3
LeoSantosRS <bigsoftsbrasil@gmail.com>
2022-12-22 19:18:53 -0300

    Aula 03
    Instalamos a biblioteca Morgan
    e fizemos as rotas de tratamento de erros

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
+app.use(morgan('dev'));
 app.use('/produtos', rotaProdutos)
 app.use('/pedidos', rotaPedidos)
 
@@ -12,5 +14,23 @@ app.use((req, res, next) => {
         mensagem: 'ok, Deu Certo'
     })    
 })
+
+// tratamento de erros
+app.use((req, res, next) => {
+   const erro = new Error('não encontrado')  
+   erro.status = 404;
+   next(erro); 
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro:{
+            mensagem: error.message
+        }
+    });
+ });
+
+
 
 module.exports = app;
